35b4dc37f86e42d3bef4344f6579de2e7dca99e0
Benard Ronoh <biikate48@gmail.com>
2023-09-20 16:18:30 +0300

    last commit

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" Amenity Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place, place_amenity
+from os import environ
 
 class Amenity(BaseModel):
-    name = ""
+    """ Amenity class """
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity)
+    else:
+        name = ""
