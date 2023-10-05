b41db56948b741481f56fdaffeca7df9d071a5dc
Benard Ronoh <biikate48@gmail.com>
2023-10-05 20:32:28 +0300

    pycodestyle

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,52 +5,60 @@ from fabric.api import *
 from datetime import datetime
 from os import path
 
-
 env.hosts = ['18.209.20.255', '34.73.76.135']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_pack():
-        """Function to compress directory
-        Return: path to archive on success; None on fail
-        """
-        # Get current time
-        now = datetime.now()
-        now = now.strftime('%Y%m%d%H%M%S')
-        archive_path = 'versions/web_static_' + now + '.tgz'
+    """Function to compress directory
+    Return: path to archive on success; None on fail
+    """
+    # Get current time
+    now = datetime.now()
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + now + '.tgz'
 
-        # Create archive
-        local('mkdir -p versions/')
-        result = local('tar -cvzf {} web_static/'.format(archive_path))
+    # Create archive
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
 
-        # Check if archiving was successful
-        if result.succeeded:
-                return archive_path
-        return None
+    # Check if archiving was successful
+    if result.succeeded:
+        return archive_path
+    return None
 
 
 def do_deploy(archive_path):
-        """Deploy web files to server
-        """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
+    """Deploy web files to server
+    """
+    try:
+        if not path.exists(archive_path):
+            return False
 
-                # upload archive
-                put(archive_path, '/tmp/')
+        # upload archive
+        put(archive_path, '/tmp/')
 
-                # create target dir
-                timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
+        # create target dir
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'
+            .format(timestamp))
 
-                # uncompress archive and delete .tgz
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-                    .format(timestamp, timestamp))
+        # uncompress archive and delete .tgz
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C '
+            '/data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
 
-                # remove archive
-                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+        # remove archive
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
-                # move contents into host web_static
+        # move contents into host web_static
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* '
+            '/data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+    except Exception as e:
+        print("Exception:", e)
+        return False
+
+    # return True on success
+    return True
