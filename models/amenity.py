398063cc76b0845aef0ec6b97f01480eb6a91c1f
Benard Ronoh <biikate48@gmail.com>
2023-10-22 12:57:15 +0300

    updates

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place, place_amenity
-from os import environ
 
-class Amenity(BaseModel):
-    """ Amenity class """
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary=place_amenity)
-    else:
-        name = ""
+
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    # place_amenities = relationship('Place', secondary='place_amenity')
